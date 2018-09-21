1424ef8a826b6c453ada5301a77257cf66f40a83
Justin Sherman <justinsherman419@yahoo.com>
2018-09-21 17:39:36 -0400

    fix comments

--- narnia1/printstack32.s
+++ narnia1/printstack32.s
@@ -5,13 +5,13 @@ section .text:
 
 _start:
     push 4                  ; push 32 bit write syscall number onto stack
-    pop eax                 ; pop 4 from the stack and put it into rax
-    xor ebx, ebx            ; clear rdi for stdout (0)
-    xor esi, esi            ; clear rdx for NULL byte
+    pop eax                 ; pop 4 from the stack and put it into eax
+    xor ebx, ebx            ; clear ebx for stdout (0)
+    xor esi, esi            ; clear esi for NULL byte
     push esi                ; push NULL byte
     push 0x0a6f6c6c         ; push "\noll"
     push 0x65680101         ; push "eh" and two SOH chars to prevent NULL bytes
-    mov ecx, esp            ; put the address of our string into rsi
+    mov ecx, esp            ; put the address of our string into ecx
     push 12                 ; push the length of our string onto the stack
-    pop edx                 ; pop the length of our string and put it into rdx
+    pop edx                 ; pop the length of our string and put it into edx
     int 0x80                ; perform write syscall
